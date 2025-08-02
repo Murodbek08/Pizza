aaeb00edd9bdcf5afe9b46f96e0a03569bc241ab
Murodbek08 <arslonovmurodbek071@gmail.com>
2025-08-02 12:29:01 +0500

    fix

--- src/context/LanguageContextProvider.jsx
+++ src/context/LanguageContextProvider.jsx
@@ -1,21 +1,36 @@
 import PropTypes from "prop-types";
-import { LanguageContext } from ".";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RU } from "../data/RU";
-import { UZ } from "../data/UZ";
+import { UZ } from "../data/uz";
+import { LanguageContext } from ".";
+
 const LanguageContextProvider = ({ children }) => {
   const [langType, setLangType] = useState(
     localStorage.getItem("language")?.toLowerCase() || "ru"
   );
+
   const language = { ru: RU, uz: UZ };
-  let state = { langType, lang: language[langType], setLangType };
+
+  // LangType o‘zgarsa — localStorage yangilanadi
+  useEffect(() => {
+    localStorage.setItem("language", langType);
+  }, [langType]);
+
+  const state = {
+    langType,
+    lang: language[langType],
+    setLangType,
+  };
+
   return (
     <LanguageContext.Provider value={state}>
       {children}
     </LanguageContext.Provider>
   );
 };
+
 LanguageContextProvider.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
+
 export default LanguageContextProvider;
